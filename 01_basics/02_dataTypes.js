581bceb180c4e2860a64b3c4e617ac8ac4671077
ahmadkhan-afzali <ahmadkhanafzaly22@gmail.com>
2025-05-05 11:01:06 +0000

    dataTypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,6 +1,6 @@
 "use strict"; // Treat all JS code as newer version
 
-alert(3+3) // we are using nodejs , not browser
+//alert(3+3) // we are using nodejs , not browser
 
 const Mobile =12345
 let  name = "Hamid"
