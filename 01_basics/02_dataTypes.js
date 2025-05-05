b880c1f2e98f0af857fdfa0c045ba143c4ef99f1
ahmadkhan-afzali <ahmadkhanafzaly22@gmail.com>
2025-05-05 08:43:55 +0000

    datat types js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,8 @@
+"use strict"; // Treat all JS code as newer version
+
+alert(3+3) // we are using nodejs , not browser
+
+const Mobile =12345
+let  name = "Hamid"
+let isLoggedin = true
+console.log(typeof(Mobile));
