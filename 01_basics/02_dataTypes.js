fecf8f49bf4039eff8387ee1e130b14e7e8671a5
ahmadkhan-afzali <ahmadkhanafzaly22@gmail.com>
2025-05-05 09:09:57 +0000

    dataTypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -5,4 +5,23 @@ alert(3+3) // we are using nodejs , not browser
 const Mobile =12345
 let  name = "Hamid"
 let isLoggedin = true
-console.log(typeof(Mobile));
+console.log(typeof(Mobile),typeof(name),typeof(isLoggedin));
+//console.log(typeof[Mobile,[name],[isLoggedin]);
+
+    // Data Types
+// number => 2 to pawer 53
+// bigint            ->big integer
+// string => ""
+// boolean => true/false
+// null  => standalone value
+// undefined =>     Ex: let name;
+// symbol => unique -> is used for finding uniqueness
+
+
+// Object
+
+console.log(typeof null); // object
+console.log(typeof undefined); //undefined
+
+
+
