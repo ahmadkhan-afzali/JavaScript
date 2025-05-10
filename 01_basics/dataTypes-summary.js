9b7e50354ac91e3056c0c6679ffe5ad3f41b5a68
ahmadkhan-afzali <ahmadkhanafzaly22@gmail.com>
2025-05-10 10:03:39 +0000

    data types summary

---
+++ 01_basics/dataTypes-summary.js
@@ -0,0 +1,15 @@
+// Data Types 
+// -> 1) Primitive 2) Reference (Non Primitive)
+
+//// 1) PRIMITIVE ////
+// 7 Types: String, Number, null, Undefined ,BigInt ,Boolean ,Symbol.
+
+const name ="Abdul"
+const mob = 7098537653
+const isLogged = false
+
+console.log(name);
+
+// 2) Reference ( Non primitive )
+// -> Array, Objects, Functions
+
